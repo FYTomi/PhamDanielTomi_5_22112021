b33815db5e6de94d6c2ce02dd72c3462e852fd8c
Pham Daniel <tomiyoshida75@gmail.com>
2021-11-24 17:12:05 +0100

    étape 3

--- front/js/script.js
+++ front/js/script.js
@@ -1,16 +1,28 @@
 // Variable fonction pour afficher les infos produit de la page d'accueil
 
 let displayProductInfo = function(data) {
-    let itemInfo = document.getElementById("items")
+    let items = document.getElementById("items")
     for (let i in data) {
-        items.innerHTML += 
+        items.innerHTML += `
         <a href="./product.html?id=${data[i]._id}">
             <article>
               <img src="${data[i].imageUrl}" alt="${data[i].altTxt}"/>
               <h3 class="productName">${data[i].name} </h3>
               <p class="productDescription">${data[i].description}</p>
             </article>
-          </a>
+          </a> `
         
     }
 }
+// Requête API pour le JSON des produits à afficher
+
+fetch('http://localhost:3000/api/products')
+  .then(function(res) {
+      if (res.ok) {
+          return res.json();
+      }
+    })
+    .then (function(data) {
+        displayProductInfo(data);
+    }
+    )
