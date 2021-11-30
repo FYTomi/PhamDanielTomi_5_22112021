197e3cc9d691a040d4f4ae4818d5f49ee5ce4642
Pham Daniel <tomiyoshida75@gmail.com>
2021-11-30 16:20:08 +0100

    fonction fléchée

--- front/js/script.js
+++ front/js/script.js
@@ -1,31 +1,28 @@
-// Variable fonction pour afficher les infos produit de la page d'accueil
 
-let displayProductInfo = function(data) {
-    let items = document.getElementById("items")
-    for (let i in data) {
-        items.innerHTML += `
-        <a href="./product.html?id=${data[i]._id}">
-            <article>
-              <img src="${data[i].imageUrl}" alt="${data[i].altTxt}"/>
-              <h3 class="productName">${data[i].name} </h3>
-              <p class="productDescription">${data[i].description}</p>
-            </article>
-          </a> `
-        
-    }
-}
 // Requête API pour le JSON des produits à afficher
 
 fetch('http://localhost:3000/api/products')
-  .then((res) => {
-      if (res.ok) {
-          return res.json();
-      }
-    })
-    .then ((data) => {
-        displayProductInfo(data);
+    .then((res) => res.json())
+    .then ((data) => { 
+        
+        // Variable fonction pour afficher les infos produit de la page d'accueil
+
+    let displayProductInfo = () => {
+        let items = document.getElementById("items")
+        for (let i in data) {
+            items.innerHTML += `
+            <a href="./product.html?id=${data[i]._id}">
+                <article>
+                  <img src="${data[i].imageUrl}" alt="${data[i].altTxt}"/>
+                  <h3 class="productName">${data[i].name} </h3>
+                  <p class="productDescription">${data[i].description}</p>
+                </article>
+              </a> `
+            
+        }
     }
-    )
+    displayProductInfo()
+})
     .catch((err) => {
         console.log ("Impossible d'afficher les articles");
     }
