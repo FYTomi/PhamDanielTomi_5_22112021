5c0918642736d574f9cd91af6249eec61663de45
Pham Daniel <tomiyoshida75@gmail.com>
2021-11-30 14:52:06 +0100

    étape 5

--- front/js/product.js
+++ front/js/product.js
@@ -1,4 +1,19 @@
 // Récupérer paramètres des URL via searchParams
     //window.location.href pour récupérer l'URL de la page
     //get pour récupérer l'id de la page
+
 let idProduit = new URL (window.location.href).searchParams.get('id')
+
+//Requête API pour le JSON des produits à afficher et s'assurer que l'ID du produit récupéré correspond à la variable idProduit
+
+fetch('http://localhost:3000/api/products')
+  .then(function(res) {
+      if (res.ok) {
+          return res.json();
+      }
+    })
+    .then (function(data) {
+        let findProduct = function() {
+            return data.find(function(object) {object._id === idProduit})
+        }
+    })
