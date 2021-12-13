e51920b6be1602b26a0aee90752f7e18a296fe5b
Pham Daniel <tomiyoshida75@gmail.com>
2021-12-13 18:42:49 +0100

    sélection et récup info du local storage

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,6 +1,8 @@
     
-    //Déclaration de la variable "produitEnregistreDansLocalStorage" dans laquelle on met les keys et les values dans le localStorage
-let produitEnregistreDansLocalStorage = JSON.parse(localStorage.getItem("produit"));
-//JSON.parse c'est pour convertir les données au format JSON ui sont dans le localStorage en objet JS
-console.log(produitEnregistreDansLocalStorage);
 
+//On récupére les infos stockées dans le localStorage
+let produitEnregistreDansLocalStorage = JSON.parse(localStorage.getItem("produit"));
+    
+
+//Sélection dans le DOM du conteneur des articles ajoutés
+let cartContainer = document.getElementById("cart__items")
