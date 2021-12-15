b72099d809d590fe76266e234875e0c22aa4a5a5
Pham Daniel <tomiyoshida75@gmail.com>
2021-12-15 12:59:24 +0100

    Verif si le panier et vide ou rempli

--- front/js/cart.js
+++ front/js/cart.js
@@ -6,3 +6,11 @@ let produitEnregistreDansLocalStorage = JSON.parse(localStorage.getItem("produit
 
 //Sélection dans le DOM du conteneur des articles ajoutés
 let cartContainer = document.getElementById("cart__items")
+
+// Si le panier est vide : Afficher le panier est vide
+
+if(produitEnregistreDansLocalStorage === null) {
+    console.log("le panier est vide");
+} else {
+    console.log("le panier possède des articles")
+}
